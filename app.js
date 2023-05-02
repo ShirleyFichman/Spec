df58d4fc19623524479feb5238718c0748003271
Shirley Fichman <shirleyfichman@gmail.com>
2023-05-02 21:58:24 +0300

    adding requirements to app.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express= require('express');
+const bodyParser = require('body-parser');
 
 const app= express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const companyRoutes = require('./routes/company');
+const userRoutes = require('./routes/user');
+const generalRoutes = require('./routes/general');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/user', userRoutes);
+app.use(companyRoutes);
+app.use(generalRoutes);
 
 app.listen(3000);
