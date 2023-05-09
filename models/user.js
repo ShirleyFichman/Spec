eb8bb4f05054ca798f9dd9927ebad9b5e5edf10d
Shirley Fichman <shirleyfichman@gmail.com>
2023-05-09 13:32:05 +0300

    Adding the basic user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const Sequelize= require('sequelize'); // the package
+
+const sequelize= require('../util/database'); // have the connection pool
+
+//the password might be changed later for safety and auth reasons and email should be checked that is email
+//should add more relevant fields like experience, summary, cv, location etc
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+});
+
+module.exports= User;
