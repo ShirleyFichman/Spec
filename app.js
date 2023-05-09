450520180bbd2f49be5afbdc629bc409ecaf11c2
Shirley Fichman <shirleyfichman@gmail.com>
2023-05-09 19:10:51 +0300

    solving the bug with the connection to db

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express= require('express');
 const bodyParser = require('body-parser');
 
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app= express();
 
@@ -23,4 +23,9 @@ app.use(generalRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result => {
+    console.log(result);
+    app.listen(3000);
+}).catch(err =>{
+    console.log(err);
+});
