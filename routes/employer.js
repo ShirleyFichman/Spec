07d01b9ceea9cb8e42ea2764286b2fa1a0709467
Shirley Fichman <shirleyfichman@gmail.com>
2023-05-15 13:25:19 +0300

    adding basic controller, route, views to employer

--- routes/employer.js
+++ routes/employer.js
@@ -6,6 +6,6 @@ const employerController = require('../controllers/employer');
 
 const router = express.Router();
 
-//all the routes will be added here
+router.use('/', employerController.getHome); // needs to be forwarded to the home page with that user signed in
 
 module.exports = router;
