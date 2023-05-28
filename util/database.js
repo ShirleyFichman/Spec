5e5f92e4c70bec06bbe781179fec03776007da18
Shirley Fichman <shirleyfichman@gmail.com>
2023-05-28 12:24:55 +0300

    correct port

--- util/database.js
+++ util/database.js
@@ -2,7 +2,8 @@ const Sequelize= require('sequelize');
 
 const sequelize= new Sequelize('spec', 'root', 'Shirley2721996', {
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'localhost', 
+    port: '3306'
 });
 
 module.exports= sequelize;
