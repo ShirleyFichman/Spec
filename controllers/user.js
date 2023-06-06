399f99b4c5921651eb8c737f5a5ba09327d73395
Shirley Fichman <shirleyfichman@gmail.com>
2023-06-06 15:14:09 +0300

    Adding get profile and edit profile pages

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,22 @@
-exports.getHome = (req, res, next) => {
+//TODO change id of profile to the real one instead of dummy
+const User = require('../models/user');
+const Profile = require('../models/profile');
+/*
+const applyUserId = (req, res, next) => {
   const userId = req.params.userId;
-    res.render('user/home', {
-      pageTitle: 'Home Page',
-      path: '/',
-      userId: userId,
-    });
+  req.user = userId;
+  console.log("user id: ", req.user);
+  res.send();
   };
+  */
+
+exports.getHome= (req, res, next) =>{
+  res.render('user/home', {
+    pageTitle: 'Home Page',
+    path: '/',
+    userId: req.params.userId,
+  });
+}
 
 exports.getJobs = (req, res, next) => {
     res.render('user/jobs', {
@@ -16,14 +27,60 @@ exports.getJobs = (req, res, next) => {
   };
 
 exports.getProfile = (req, res, next) => {
-  const userId = req.params.userId;
-  const profile= Profile.findByPk(userId);
-    res.render('user/profile', {
-      pageTitle: 'Profile Page',
-      path: '/profile',
-      userId: userId,
-      profile: profile,
-      editing: false
-    });
+  const userId = 1;
+  User.findByPk(userId).then(user=>{
+    user.getProfile()
+    .then(profile =>{
+      res.render('user/profile', {
+        pageTitle: 'Profile Page',
+        path: '/profile',
+        profile: profile,
+        editing: false,
+      });
+    })
+    .catch(err => console.log(err))
+  })
+  .catch(err => console.log(err))
   };
+
+
+exports.getEditProfile = (req, res, next) => {
+  console.log("in edit profile");
+  const userId = 1;
+  User.findByPk(userId).then(user=>{
+    user.getProfile()
+    .then(profile =>{
+      res.render('user/edit-profile', {
+        pageTitle: 'Edit Profile Page',
+        path: '/profile',
+        profile: profile,
+        editing: true,
+      });
+    })
+    .catch(err => console.log(err))
+  })
+  .catch(err => console.log(err))
+  };
+
+
+  exports.postEditProfile= (req, res, next) => {
+    const profileId = 1;
+    const updatedFullName = req.body.fullName;
+    const updatedLocation = req.body.location;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedIntro = req.body.intro;
+    Profile.findByPk(profileId)
+      .then(profile => {
+        profile.fullNAme = updatedFullName;
+        profile.location = updatedLocation;
+        profile.imageUrl = updatedImageUrl;
+        profile.intro = updatedIntro;
+        return profile.save();
+      })
+      .then(result => {
+        console.log('updated profile');
+        res.redirect('/user/profile/'+profileId);
+      })
+      .catch(err => console.log(err));
+  }
   
