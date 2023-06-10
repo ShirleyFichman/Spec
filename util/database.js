7245106ba3048f8b939686ef2dd27dcc4ccd9027
Shirley Fichman <shirleyfichman@gmail.com>
2023-06-10 12:20:44 +0300

    changing name to job

--- util/database.js
+++ util/database.js
@@ -3,7 +3,6 @@ const Sequelize= require('sequelize');
 const sequelize= new Sequelize('spec', 'root', 'Shirley2721996', {
     dialect: 'mysql',
     host: 'localhost'
-    //port: '3306'
 });
 
 module.exports= sequelize;
