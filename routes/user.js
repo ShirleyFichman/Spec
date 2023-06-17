03eb9ae38ebd752f938936e4ac4ee4c920418319
Shirley Fichman <shirleyfichman@gmail.com>
2023-06-17 15:25:31 +0300

    adding resume model and the option to upload resume

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const express = require('express');
+const multer = require('multer');
 
 const userController = require('../controllers/user');
+
+const upload = multer({ dest: 'uploads/' }); 
 
 const router = express.Router();
 
@@ -9,6 +12,7 @@ router.use('/jobs/apply-for-job/:userId/:jobId', userController.postApply);
 router.use('/jobs/:userId', userController.getJobs); 
 router.get('/get-edit-profile/:userId', userController.getEditProfile);
 router.post('/post-edit-profile/:userId', userController.postEditProfile);
+router.post('/profile/upload-resume/:userId', upload.single('resume'), userController.postResume); 
 router.use('/profile/:userId', userController.getProfile); 
 router.get('/home/:userId', userController.getHome); 
 
